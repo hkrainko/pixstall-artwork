a49c7a008cfbb1e51f491e2ca6bc207d7b838f20
hkrainko <>
2021-04-04 01:33:25 +0800

    no message

--- app/msg-broker/repo/rabbitmq/msg/updated_artwork.go
+++ app/msg-broker/repo/rabbitmq/msg/updated_artwork.go
@@ -1,5 +1,39 @@
 package msg
 
+import (
+	"pixstall-artwork/domain/artwork/model"
+	"time"
+)
+
 type UpdatedArtwork struct {
+	ID                   string  `json:"id"`
+	ArtistName           *string `json:"artistName"`
+	ArtistProfilePath    *string `json:"artistProfilePath"`
+	RequesterName        *string `json:"requesterName"`
+	RequesterProfilePath *string `json:"requesterProfilePath"`
+
+	Title       *string `json:"title"`
+	TextContext *string `json:"textContext"`
+	Views       *int    `json:"views"`
+	FavorCount  *int    `json:"favorCount"`
+
+	LastUpdateTime *time.Time          `json:"lastUpdateTime"`
+	State          *model.ArtworkState `json:"state"`
+}
+
+func NewUpdatedArtwork(updater model.ArtworkUpdater, views *int, favorCount *int) UpdatedArtwork {
+	return UpdatedArtwork{
+		ID:                   updater.ID,
+		ArtistName:           updater.ArtistName,
+		ArtistProfilePath:    updater.ArtistProfilePath,
+		RequesterName:        updater.RequesterName,
+		RequesterProfilePath: updater.RequesterProfilePath,
+		Title:                updater.Title,
+		TextContext:          updater.TextContext,
+		Views:                views,
+		FavorCount:           favorCount,
+		LastUpdateTime:       updater.LastUpdateTime,
+		State:                updater.State,
+	}
 
 }
