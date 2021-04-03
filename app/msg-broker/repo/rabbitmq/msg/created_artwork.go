a1ac49f67790db61bb728ccc33add4ca70640eb5
hkrainko <>
2021-04-03 17:39:13 +0800

    no message

--- app/msg-broker/repo/rabbitmq/msg/created_artwork.go
+++ app/msg-broker/repo/rabbitmq/msg/created_artwork.go
@@ -7,23 +7,25 @@ import (
 
 type CreatedArtwork struct {
 	ID                   string             `json:"id" bson:"id"`
-	CommissionID         string             `json:"commissionId" bson:"commissionId"`
-	OpenCommissionID     string             `json:"openCommissionId" bson:"openCommissionId"`
-	ArtistID             string             `json:"artistId" bson:"artistId"`
-	ArtistName           string             `json:"artistName" bson:"artistName"`
-	ArtistProfilePath    *string            `json:"artistProfilePath" bson:"artistProfilePath,omitempty"`
-	RequesterID          string             `json:"requesterId" bson:"requesterId"`
-	RequesterName        string             `json:"requesterName" bson:"requesterName"`
-	RequesterProfilePath *string            `json:"requesterProfilePath" bson:"requesterProfilePath,omitempty"`
-	IsR18                bool               `json:"isR18" bson:"isR18"`
-	Anonymous            bool               `json:"anonymous" bson:"anonymous"`
+	CommissionID         string             `json:"commissionId"`
+	OpenCommissionID     string             `json:"openCommissionId"`
+	ArtistID             string             `json:"artistId"`
+	ArtistName           string             `json:"artistName"`
+	ArtistProfilePath    *string            `json:"artistProfilePath,omitempty"`
+	RequesterID          string             `json:"requesterId"`
+	RequesterName        string             `json:"requesterName"`
+	RequesterProfilePath *string            `json:"requesterProfilePath,omitempty"`
+	DayUsed              time.Duration      `json:"dayUsed"`
+	IsR18                bool               `json:"isR18"`
+	Anonymous            bool               `json:"anonymous"`
 	Path                 string             `json:"path"`
 	Volume               int64              `json:"volume"`
 	Size                 model.Size         `json:"size"`
 	ContentType          string             `json:"contentType"`
-	Rating               int                `json:"rating,omitempty" bson:"rating,omitempty"`
-	CompletedTime        time.Time          `json:"completedTime" bson:"completedTime,omitempty"`
-	State                model.ArtworkState `json:"state" bson:"state"`
+	Rating               int                `json:"rating"`
+	StartTime            time.Time          `json:"startTime"`
+	CompletedTime        time.Time          `json:"completedTime"`
+	State                model.ArtworkState `json:"state"`
 }
 
 func NewCreatedArtwork(artwork model.Artwork) CreatedArtwork {
@@ -37,6 +39,7 @@ func NewCreatedArtwork(artwork model.Artwork) CreatedArtwork {
 		RequesterID:          artwork.RequesterID,
 		RequesterName:        artwork.RequesterName,
 		RequesterProfilePath: artwork.RequesterProfilePath,
+		DayUsed:              artwork.DayUsed,
 		IsR18:                artwork.IsR18,
 		Anonymous:            artwork.Anonymous,
 		Path:                 artwork.Path,
---
+++ app/msg-broker/repo/rabbitmq/msg/updated_artwork.go
@@ -0,0 +1,5 @@
+package msg
+
+type UpdatedArtwork struct {
+
+}
